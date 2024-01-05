3c9aa4b94f1efee516124c71e6e379eeff2c1611
Prathamesh <prathameshmane855@gmail.com>
2024-01-05 20:01:35 +0530

    Day 1 Node js added

---
+++ Day1-Introduction/main.js
@@ -0,0 +1,11 @@
+// js code run in Browser because of JS Engine, NodeJs is Runtime environment for JS 
+// chrome have v8 engine JS Engine , firefox uses spidermonkey
+// LTS - Long Term Support , even for stable release and odd for beta users
+// npm is node package manager(helps to package installation management)
+// npm init - created a config file (package json) ()
+
+// DOM related function are removed while creating NodeJS ( server side needed things are there)
+
+
+
+console.log("Hello world")
